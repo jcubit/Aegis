e89364f352e7b3336626e437f473340e77b2599d
jcubit <jav.cuesta@gmail.com>
2023-03-16 20:11:30 +0100

    Initial Commit

---
+++ Aegis/AegisApp.swift
@@ -0,0 +1,17 @@
+//
+//  AegisApp.swift
+//  Aegis
+//
+//  Created by Javier Cuesta on 16.03.23.
+//
+
+import SwiftUI
+
+@main
+struct AegisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
